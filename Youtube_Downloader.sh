f2e1c82079620fbf50245fcbc62532c0fd3a2974
bblinder <filmmaven@gmail.com>
2022-02-03 23:41:17 -0500

    modified:   Youtube_Downloader.sh

--- Youtube_Downloader.sh
+++ Youtube_Downloader.sh
@@ -3,6 +3,34 @@
 set -euo pipefail
 shopt -s nullglob
 shopt -s nocaseglob
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  # script cleanup here
+}
+
+setup_colors() {
+  if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+    NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+  else
+    NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+  fi
+}
+
+msg() {
+  echo >&2 -e "${1-}"
+}
+
+die() {
+  local msg=$1
+  local code=${2-1} # default exit status 1
+  msg "$msg"
+  exit "$code"
+}
+
 
 mp3_convert() {
 	for fname in ./*.{m4a,webm,opus,ogg,mp3}; do
@@ -45,6 +73,8 @@ remove_originals() {
 		;;
 	esac
 }
+
+setup_colors
 
 YT_DL "$@"
 mp3_convert
