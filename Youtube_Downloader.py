fc3aa87f1f150706b678fab420e5d88ff6af14bf
bblinder <filmmaven@gmail.com>
2022-03-07 12:42:05 -0500

    modified:   Youtube_Downloader.py

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -29,7 +29,7 @@ class MyLogger(object):
 
 def my_hook(d):
 	if d['status'] == 'finished':
-		print('Done downloading, now converting...')
+		print('::: Done downloading, now converting...')
 
 ydl_opts = {
 	'writethumbnail': True,
