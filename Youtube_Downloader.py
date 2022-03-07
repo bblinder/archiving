307c8ef6af795bdc09c6b6b8105c85956becac9f
bblinder <filmmaven@gmail.com>
2022-03-07 12:40:45 -0500

    modified:   Youtube_Downloader.py 	modified:   requirements.txt

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -3,14 +3,20 @@
 # Downloads YouTube Videos and converts them via FFmpeg into mp3s.
 
 from __future__ import unicode_literals
-import sys
+
 try:
 	from yt_dlp import YoutubeDL
 except ImportError:
-	print("YouTube-DLP not found.")
-	print("Please ensure it's installed.")
+	print("::: YouTube-DLP not found.")
+	print("::: Please ensure it's installed.")
 
-YT_URL = sys.argv[1]
+try:
+	from halo import Halo
+except ImportError:
+	print("::: Halo not installed - please install with: ")
+	print("::: 'pip install halo' ")
+
+#YT_URL = sys.argv[1]
 class MyLogger(object):
 	def debug(self, msg):
 		pass
@@ -36,6 +42,16 @@ ydl_opts = {
 	'logger': MyLogger(),
 	'progress_hooks': [my_hook],
 }
+def download_url(url):
+	with YoutubeDL(ydl_opts) as ydl:
+		ydl.download([url])
 
-with YoutubeDL(ydl_opts) as ydl:
-	ydl.download([YT_URL])
+
+if __name__ == '__main__':
+	import argparse
+	parser = argparse.ArgumentParser(description="Downloads YouTube Videos and converts them via FFmpeg into mp3s.")
+	parser.add_argument("URL", help="The URL of the track to download")
+	args = parser.parse_args()
+	url = args.URL
+
+	download_url(url)
