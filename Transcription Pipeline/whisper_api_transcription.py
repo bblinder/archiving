397f667ea71179607a39da9387a7a8ef0bfb8ac6
bblinder <filmmaven@gmail.com>
2023-03-27 14:43:36 -0400

    modified:   whisper_api_transcription.py

--- Transcription Pipeline/whisper_api_transcription.py
+++ Transcription Pipeline/whisper_api_transcription.py
@@ -21,13 +21,6 @@ from pydub import AudioSegment
 from tqdm import tqdm
 from yt_dlp import YoutubeDL
 
-if not Path("config.yaml").is_file():
-    raise FileNotFoundError(
-        "config.yaml not found. Please create a config.yaml file in the same directory as the script."
-    )
-
-with open("config.yaml", "r") as ymlfile:
-    config = yaml.safe_load(ymlfile)
 
 logging.basicConfig(
     format="%(asctime)s %(levelname)s %(message)s",
@@ -41,15 +34,14 @@ def download_video(url, output_dir):
     """
     Download the video at 48kbps as a mp3, and retain thumbnail and metadata.
     """
-
     os.makedirs(output_dir, exist_ok=True)
 
     info = YoutubeDL().extract_info(url, download=False)
     title = info["title"]
-    path_to_file = Path(output_dir) / f"{title}.mp3".strip()
+    path_to_file = Path(output_dir) / f"{title}.mp3"
     if path_to_file.is_file():
         logger.info(f"{title} already downloaded. Skipping this part...")
-        return str(path_to_file)
+        return path_to_file
 
     ydl_opts = {
         "outtmpl": os.path.join(output_dir, "%(title)s.%(ext)s"),
@@ -73,7 +65,11 @@ def download_video(url, output_dir):
     with YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
-    return str(path_to_file)
+    # outputting the mp3 file size
+    file_size_in_bytes = os.path.getsize(path_to_file)
+    logger.info(f"Downloaded file size: {file_size_in_bytes / 1024 / 1024:.2f} MB")
+
+    return path_to_file
 
 
 def split_audio(path_to_file, output_dir, chunk_size_in_bytes=10 * 1024 * 1024):
@@ -85,7 +81,7 @@ def split_audio(path_to_file, output_dir, chunk_size_in_bytes=10 * 1024 * 1024):
         title = Path(path_to_file).stem
         audio = AudioSegment.from_file(path_to_file, "mp3")
     except Exception as e:
-        print(f"An error occurred while reading the audio file: {e}")
+        logger.error(f"An error occurred while reading the audio file: {e}")
         return
 
     # Estimate the average bitrate of the audio file
@@ -97,31 +93,25 @@ def split_audio(path_to_file, output_dir, chunk_size_in_bytes=10 * 1024 * 1024):
     chunk_duration_in_milliseconds = 8 * chunk_size_in_bytes / estimated_bitrate
 
     num_chunks = math.ceil(duration_in_milliseconds / chunk_duration_in_milliseconds)
-    print(f"Splitting {title} into {num_chunks} segments.")
+    logger.info(f"Splitting {title} into {num_chunks} segments.")
 
     for i in tqdm(range(num_chunks)):
         start = int(i * chunk_duration_in_milliseconds)
         end = int((i + 1) * chunk_duration_in_milliseconds)
         segment = audio[start:end]
-        segment.export(f"{title}_prepared_{i}.mp3", format="mp3")
-
-    # move the chunks into a new directory
-    os.makedirs(Path(output_dir) / title, exist_ok=True)
-    for i in range(num_chunks):
-        os.rename(
-            f"{title}_prepared_{i}.mp3",
-            str(Path(output_dir) / title / f"{title}_prepared_{i}.mp3"),
+        segment.export(
+            Path(output_dir) / title / f"{title}_prepared_{i}.mp3", format="mp3"
         )
 
     # return the path to the first chunk
-    return str(Path(output_dir) / title / f"{title}_prepared_0.mp3")
+    return Path(output_dir) / title / f"{title}_prepared_0.mp3"
 
 
-def transcribe_audio(path_to_file, output_dir):
+def transcribe_audio(path_to_file, output_dir, config):
     openai.api_key = config["openai_api_key"]
     os.makedirs(output_dir, exist_ok=True)
     title = Path(path_to_file).stem
-    print(f"Transcribing {title}...")
+    logger.info(f"Transcribing {title}...")
 
     params = {
         "model": "whisper-1",
@@ -138,7 +128,7 @@ def transcribe_audio(path_to_file, output_dir):
                 raise ValueError("Empty transcript received from API")
             return transcript
         except Exception as e:
-            print(f"An error occurred while transcribing the audio file: {e}")
+            logger.error(f"An error occurred while transcribing the audio file: {e}")
             return None
 
     # if the file is 25MB or larger, split it into 10MB segments
@@ -153,14 +143,14 @@ def transcribe_audio(path_to_file, output_dir):
         transcripts = []
         previous_transcript = ""
 
-        for chunk_path in chunk_paths:
+        for i, chunk_path in tqdm(enumerate(chunk_paths), total=len(chunk_paths)):
             # Append the previous transcript to the prompt for the next segment
             params["prompt"] = f"{params['prompt']}\n{previous_transcript}"
             transcript = transcribe_single_file(chunk_path, params)
             if transcript:
                 transcripts.append(transcript)
                 previous_transcript = transcript["text"]
-
+        
         # Concatenate the transcripts
         full_transcript = {
             "text": "\n".join([t["text"] for t in transcripts]),
@@ -168,7 +158,6 @@ def transcribe_audio(path_to_file, output_dir):
         }
         for t in transcripts:
             full_transcript["segments"].extend(t["segments"])
-
     else:
         transcript = transcribe_single_file(path_to_file, params)
         full_transcript = transcript if transcript else {"text": "", "segments": []}
@@ -184,6 +173,7 @@ def transcribe_audio(path_to_file, output_dir):
         f.write(full_transcript["text"])
 
     return text_path
+
 
 
 def main():
@@ -220,21 +210,27 @@ def main():
         path_to_file = download_video(args.url, args.output_dir)
         logger.info(f"Downloaded {path_to_file}")
 
+    # check if the config file exists
+    if args.config:
+        if not Path(args.config).exists():
+            raise ValueError(f"Config file {args.config} does not exist.")
+        else:
+            config_path = args.config
+    else:
+        config_path = "config.yaml"
+
+    # load the config file
+    with open(config_path, "r") as f:
+        config = yaml.safe_load(f)
+
     # if the file is 25MB or larger, split it into 10 minute segments
     if Path(path_to_file).stat().st_size > 25 * 1024 * 1024:
         path_to_file = split_audio(path_to_file, args.output_dir)
         logger.info(f"Split {path_to_file}")
 
-    transcription = str(transcribe_audio(path_to_file, args.output_dir))
+    transcription = str(transcribe_audio(path_to_file, args.output_dir, config))
 
-    # if they exist, delete the audio file and the split audio files
-    delete_chunks_folder = input(
-        "Delete segmented audio chunks folder? (y/n): "
-    ).lower()
-    if delete_chunks_folder == "y":
-        shutil.rmtree(Path(path_to_file).parent)
-        logger.info(f"Deleted {Path(path_to_file).parent}")
-
+ 
     # ask to delete the original audio file
     delete_audio = input("Delete original audio file? (y/n): ").lower()
     if delete_audio == "y":
@@ -242,7 +238,6 @@ def main():
         logger.info(f"Deleted {path_to_file}")
 
     logger.info("Done!")
-
 
 if __name__ == "__main__":
     main()
