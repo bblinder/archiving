b5c3b52eb53716cc4e55f38c32a14f099aac3349
bblinder <filmmaven@gmail.com>
2023-03-30 20:54:59 -0400

    modified:   vtt_to_transcript.py

--- vtt_to_transcript.py
+++ vtt_to_transcript.py
@@ -1,36 +1,47 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Convert a WebVTT subtitle file to a formatted-ish transcript.
+"""
+
+import argparse
+import re
 
 import webvtt
-import re
-import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input", help="Input file")
 args = parser.parse_args()
 
+
 def capitalize_first_letter(text):
-    return re.sub(r'(^|\.\s+)(\w)', lambda m: m.group(1) + m.group(2).upper(), text)
+    """
+    Capitalize the first letter of each sentence in a string.
+    """
+    return re.sub(r"(^|\.\s+)(\w)", lambda m: m.group(1) + m.group(2).upper(), text)
+
 
 input_file = args.input
 output_title = input_file.split(".")[0]
 output_file = f"{output_title}_formatted.txt"
 
 vtt = webvtt.read(input_file)
-transcript = ""
+TRANSCRIPT = ""
 
 lines = []
 for line in vtt:
     lines.extend(line.text.strip().splitlines())
 
-previous = None
+PREVIOUS = None
 for line in lines:
-    if line == previous:
-       continue
-    transcript += " " + line
-    previous = line
+    if line == PREVIOUS:
+        continue
+    TRANSCRIPT += " " + line
+    PREVIOUS = line
 
-transcript = transcript.strip()
-transcript = capitalize_first_letter(transcript)
+TRANSCRIPT = TRANSCRIPT.strip()
+TRANSCRIPT = capitalize_first_letter(TRANSCRIPT)
 
-with open(output_file, "w") as f:
-    f.write(transcript)
+with open(output_file, "w", encoding='utf-8') as f:
+    f.write(TRANSCRIPT)
