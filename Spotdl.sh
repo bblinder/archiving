deb032789ee9e3e022d5d2e952162ca77ae4cea3
bblinder <bblinder@Blindy-Desktop>
2023-04-11 20:05:47 -0400

    Added GetOpts

--- Spotdl.sh
+++ Spotdl.sh
@@ -4,6 +4,18 @@ set -Eeuo pipefail
 
 COMMANDS=("spotdl" "yt-dlp")
 
+OUTPUT_FORMAT='{artist} - {title}'
+HELP_MSG="Usage: $(basename "$0") [options] arguments
+
+Options:
+  -o, --output FORMAT    Set the output format for downloaded files (optional)
+  -h, --help             Display this help message and exit
+
+Examples:
+  $(basename "$0") https://open.spotify.com/track/0VjIjW4GlUZAMYd2vXMi3b
+  $(basename "$0") -o '{artist}/{album}/{track_number} - {title}' https://open.spotify.com/track/0VjIjW4GlUZAMYd2vXMi3b
+"
+
 python_check() {
     if ! command -v python3 &> /dev/null; then
         echo "Python 3 not found. Please install Python 3."
@@ -18,9 +30,7 @@ python_check() {
     done
 }
 
-
 system_check() {
-    # Check if the commands are installed as system commands (e.g. Homebrew)
     for cmd in "${COMMANDS[@]}"; do
         if ! command -v "$cmd" &> /dev/null; then
             echo "$cmd not found. Please install with a package manager, e.g. brew install $cmd."
@@ -29,14 +39,31 @@ system_check() {
     done
 }
 
-
 # if python_check passes, then system_check will not be called
 python_check || system_check
 
-python3 -m spotdl "$@" --output '{artist} - {title}'
+while getopts ":o:h" opt; do
+    case ${opt} in
+        o)
+            OUTPUT_FORMAT="$OPTARG"
+            ;;
+        h)
+            echo "$HELP_MSG"
+            exit 0
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            echo "$HELP_MSG"
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            echo "$HELP_MSG"
+            exit 1
+            ;;
+    esac
+done
 
+shift $((OPTIND - 1))
 
-## TODO
-## use getops to provide options
-## See: https://www.redhat.com/sysadmin/arguments-options-bash-scripts
-
+python3 -m spotdl "$@" --output "$OUTPUT_FORMAT"
