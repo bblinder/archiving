19fc984fc46fd1f2adb8366d0c28e4169f57bad8
bblinder-splunk <bblinderman@splunk.com>
2025-03-31 14:32:02 -0400

    modified:   ExtractLinks.py 	deleted:    srt_to_text.py

--- ExtractLinks.py
+++ ExtractLinks.py
@@ -1,29 +1,38 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S uv run --script
+# /// script
+# dependencies = ["httpx", "bs4"]
+# ///
 
-# simple script to extract links from a web page.
+"""A simple utility to extract links from a webpage."""
 
 import argparse
-
-import requests
+import httpx
 from bs4 import BeautifulSoup
 
-
-# @Gooey
 def extract_links():
-    parser = argparse.ArgumentParser(description="List the URLs on a page")
-    parser.add_argument(
-        "url", help="The URL of the page we're extracting links from", type=str
-    )
-    args = vars(parser.parse_args())
+   parser = argparse.ArgumentParser(description="List the URLs on a page")
+   parser.add_argument(
+       "url", help="The URL of the page we're extracting links from", type=str
+   )
+   args = vars(parser.parse_args())
 
-    url = args["url"]
-    reqs = requests.get(url)
-    soup = BeautifulSoup(reqs.text, "html.parser")
+   url = args["url"]
+   try:
+       response = httpx.get(url)
+       response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
+       soup = BeautifulSoup(response.text, "html.parser")
 
-    urls = []
-    for link in soup.find_all("a"):
-        print(link.get("href"))
+       urls = []
+       for link in soup.find_all("a"):
+           href = link.get("href")
+           if href:
+               print(href)
+
+   except httpx.HTTPStatusError as e:
+       print(f"HTTP error occurred: {e}")
+   except Exception as e:
+       print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
-    extract_links()
+   extract_links()
