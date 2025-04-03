116605f97d1c4d6f909f23c8d0eba9def292c3d1
bblinder-splunk <bblinderman@splunk.com>
2025-04-03 09:21:43 -0400

    modified:   link_crawler.py

--- link_crawler.py
+++ link_crawler.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S uv run --script
+# /// script
+# dependencies = [
+#    "requests",
+#    "beautifulsoup4",
+#    "colorama",
+# ]
+# ///
 
 import requests
 from urllib.parse import urlparse, urljoin
